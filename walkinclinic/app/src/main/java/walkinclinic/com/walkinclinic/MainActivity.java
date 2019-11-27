d569db2c7b11bab06a6c273866bb625453142977
Sultan Oloyede <sultanoloyede@Bolajis-MacBook-Air.local>
2019-11-27 16:27:26 -0500

    Added my UI designs and re-designed profile page

--- walkinclinic/app/src/main/java/walkinclinic/com/walkinclinic/MainActivity.java
+++ walkinclinic/app/src/main/java/walkinclinic/com/walkinclinic/MainActivity.java
@@ -1,16 +1,31 @@
 package walkinclinic.com.walkinclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     Button registerButton;
     Button signinButton;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        map = googleMap;
+
+        LatLng uOttawa = new LatLng(45.4231, -75.6831);
+        map.addMarker(new MarkerOptions().position(uOttawa).title("uOttawa"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(uOttawa));
     }
 
     public void openRegister ()
